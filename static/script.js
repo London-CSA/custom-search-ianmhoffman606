74ed7d22451b9eab11d4fbc0fae869fc205ddfb8
Ian Hoffman <95450254+ianmhoffman606@users.noreply.github.com>
2024-04-22 15:56:07 +0000

    Submission

--- static/script.js
+++ static/script.js
@@ -1,1 +1,16 @@
+function toggleButton(){
+    if(document.getElementById("txtSearch").value == "")
+    {
+        document.getElementById("btnSearch").disabled = true;
+    }
+    else{
+        document.getElementById("btnSearch").disabled = false;
+    }
+}
 
+function getDate(){
+    // TBD display current date in the span tag with id="date" in web page footer
+    // hint: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
+    const d = new Date();
+    document.getElementById("date").innerHTML = d;
+}
